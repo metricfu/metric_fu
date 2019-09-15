5239a1b592f436413fa23c0225c899052dfdc7da
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-09-15 01:03:19 -0400

    Use MetricFu.with_run_dir to avoid flakiness
    This scenario is not the flaky one, but the fact that this is expecting things to work because there was a file under spec/dummy is problematic for other scenarios.

--- spec/metric_fu/loader_spec.rb
+++ spec/metric_fu/loader_spec.rb
@@ -4,7 +4,10 @@ describe MetricFu do
   it "loads the .metrics file" do
     # Global only for testing that this file gets loaded
     $metric_file_loaded = false
-    MetricFu.loader.load_user_configuration
+    MetricFu.with_run_dir Dir["spec/support"] do
+      MetricFu.loader.load_user_configuration
+    end
+
     expect($metric_file_loaded).to be_truthy
   end
 end
