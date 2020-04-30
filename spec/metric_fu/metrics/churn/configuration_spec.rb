919d27520df236df42b0e15c83c556ca18a57c9b
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2020-04-30 11:09:33 -0400

    Update expectation for ignore_files value (churn)
    Using the current default is not working as expected.

--- spec/metric_fu/metrics/churn/configuration_spec.rb
+++ spec/metric_fu/metrics/churn/configuration_spec.rb
@@ -6,7 +6,7 @@ describe MetricFu::Configuration, "for churn" do
     it "should set @churn to {}" do
       load_metric "churn"
       expect(MetricFu::Metric.get_metric(:churn).run_options).to eq(
-               start_date: '"1 year ago"', minimum_churn_count: 10, ignore_files: [], data_directory: MetricFu::Io::FileSystem.scratch_directory("churn")
+               start_date: '"1 year ago"', minimum_churn_count: 10, ignore_files: nil, data_directory: MetricFu::Io::FileSystem.scratch_directory("churn")
       )
     end
   end
